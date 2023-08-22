7139f3488ec646c3b2f7c89e4cb0be3b8926aac1
tomiban <81537786+tomiban@users.noreply.github.com>
2023-08-22 02:47:44 -0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer";
+import Contacto from "../components/Contacto";
+import Hero from "../components/Hero";
+import Gallery from "../components/Gallery";
+import Nosotros from "../components/Nosotros";
+
+const Home = () => {
+	return (
+		<>
+			<Hero />
+		</>
+	);
+};
+export default Home;
