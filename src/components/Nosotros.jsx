3973a582f2955110633d155f0fc2e8816d5a1eee
tomiban <81537786+tomiban@users.noreply.github.com>
2023-08-28 17:17:53 -0300

    nosotros

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,29 +1,37 @@
-import { dataNosotros } from "../data/data.js";
+import { data } from "../data/data.js";
 import { BsClipboardCheck } from "react-icons/bs";
 import { HiOutlineLightBulb } from "react-icons/hi";
 import { PiChair } from "react-icons/pi";
 import Stats from "./Stats.jsx";
 const Nosotros = () => {
-	const [{ first, second },  { cards }] = dataNosotros;
-
+	const [{ nosotros }] = data;
+	const {first, second, cards} = nosotros
 	return (
-		<section className='bg-gradient-to-r from-gray-100 to-gray-300 text-white'>
-				<Stats />
+		<section className=' text-white'>
+			<Stats />
 			<div className='mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 p-8'>
 				<div className='mx-auto max-w-2xl text-center font-sans px-4'>
 					<h2 className='text-3xl font-bold sm:text-5xl text-[#001A49]'>
 						Nosotros
 					</h2>
 
-					<p className='py-4 text-gray-800 text-xl text-center   whitespace-pre-line'>
-						<span>{first}</span> <span className="hidden md:inline" >{second}</span>
-					</p>
-					<span className="bg-slate-800 w-20 "></span>
+					<div className='mb-4 flex items-center flex-col sm:mb-8 md:mb-8'>
+						<p className='hidden max-w-screen-sm text-xl text-center text-gray-500 md:block mb-8'>
+								
+						</p>
+					</div>
+					
 				</div>
+
+				{/* <div className='mb-4 flex items-center flex-col sm:mb-8 md:mb-8'>
+					<p className='hidden max-w-screen-sm text-xl text-center text-gray-500 md:block mb-8'>
+						{title}
+					</p> */}
 
 				<div className='mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3'>
 					{cards.map((card, index) => (
-						<li key={index}
+						<li
+							key={index}
 							className='block rounded-xl border border-gray-800 p-8 shadow-lg transition hover:border-orange-900/80 hover:shadow-orange-500/30 cursor-pointer'
 							href='/services/digital-campaigns'>
 							{card.icon === "BsClipboardCheck" && (
@@ -54,7 +62,6 @@ const Nosotros = () => {
 						</li>
 					))}
 				</div>
-
 			</div>
 		</section>
 	);
